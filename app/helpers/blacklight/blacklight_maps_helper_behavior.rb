cee8f1be8300f657cb35a6fb2aa79bcd83d90a66
Richard Peng <richard@richardpeng.com>
2018-06-04 13:12:27 -0400

    Use strong parameters to filter placename_field link params

--- app/helpers/blacklight/blacklight_maps_helper_behavior.rb
+++ app/helpers/blacklight/blacklight_maps_helper_behavior.rb
@@ -34,13 +34,12 @@ module Blacklight::BlacklightMapsHelperBehavior
   # create a link to a location name facet value
   def link_to_placename_field field_value, field, displayvalue = nil
     if params[:f] && params[:f][field] && params[:f][field].include?(field_value)
-      new_params = params
+      new_params = params.permit(:coordinates, :spatial_search_type, :view, :controller, :action)
     else
       new_params = search_state.add_facet_params(field, field_value)
     end
     new_params[:view] = default_document_index_view_type
-    link_to(displayvalue.presence || field_value,
-            search_catalog_path(new_params.except(:id, :spatial_search_type, :coordinates)))
+    link_to(displayvalue.presence || field_value, search_catalog_path(new_params))
   end
 
   # create a link to a spatial search for a set of point coordinates
